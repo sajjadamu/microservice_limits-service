d8d2b959b512dc3731a774d5c5aac2dab5e07850
Sajjad <sajjad.hussain@indicsoft.com>
2020-03-19 17:16:27 +0530

    Spring Cloud bus -> for making a single refresing to effect all request for getting properties value

--- src/main/java/com/in28minutes/microservices/limitsservice/LimitConfigurationController.java
+++ src/main/java/com/in28minutes/microservices/limitsservice/LimitConfigurationController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.in28minutes.microservices.limitsservice.bean.LimitConfiguration;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 public class LimitConfigurationController {
@@ -17,4 +18,13 @@ public class LimitConfigurationController {
 		return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
 	}
 
+	@GetMapping("fault-tolerance-example")
+	@HystrixCommand(fallbackMethod="fallbackRetrievConfiguration")
+	public LimitConfiguration retrievConfiguration(){
+		throw new RuntimeException("not Available");
+	}
+	
+	public LimitConfiguration fallbackRetrievConfiguration(){
+		return new LimitConfiguration(999, 9);
+	}
 }
