d85635c5b10e144754f47d8faec19f488e402d42
sajjad <sajjad@ansari.mshome.net>
2021-11-29 16:15:53 +0530

    cleanup

--- src/main/java/com/in28minutes/microservices/limitsservice/LimitsServiceApplication.java
+++ src/main/java/com/in28minutes/microservices/limitsservice/LimitsServiceApplication.java
@@ -2,10 +2,9 @@ package com.in28minutes.microservices.limitsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
-@EnableHystrix
+//@EnableHystrix
 public class LimitsServiceApplication {
 
 	public static void main(String[] args) {
